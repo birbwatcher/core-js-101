0604230521033ecee16201ef33768106946a90a6
birbwatcher <d@rkmind.cc>
2022-10-19 21:54:54 +0200

    feat: 48 tasks passed

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -86,8 +86,8 @@ function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot(/* a, b */) {
-  throw new Error('Not implemented');
+function getLinearEquationRoot(a, b) {
+  return -b / a;
 }
 
 
@@ -201,8 +201,11 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n === 2 || n === 3 || n === 7) return true;
+  if (n % 2 !== 0 && n % 3 !== 0 && n % 7 !== 0) {
+    return true;
+  } return false;
 }
 
 /**
