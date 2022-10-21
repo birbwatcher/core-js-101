865c57ea9ea838c020b6a6b1d10658ee9fe84ffd
birbwatcher <d@rkmind.cc>
2022-10-21 15:33:00 +0200

    feat: 61 tasks solved

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -52,8 +52,12 @@ function getFizzBuzz(num) {
  *   5  => 120
  *   10 => 3628800
  */
-function getFactorial(/* n */) {
-  throw new Error('Not implemented');
+function getFactorial(n) {
+  let result = 1;
+  for (let i = n; i > 0; i -= 1) {
+    result *= i;
+  }
+  return result;
 }
 
 
